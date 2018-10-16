786dba7ec54f07ef96653e7feeb9d29373086f44
Alexey Petrov <ruddy22@mail.ru>
2018-10-17 01:18:01 +0300

    [server]Add lightweight server and control on it
    Added `httpkit.server` to the project
    `start-server` and `stop-server` fns have been implemented

--- src/flocktory/core.clj
+++ src/flocktory/core.clj
@@ -1,6 +1,44 @@
-(ns flocktory.core)
+(ns flocktory.core
+  (:require [org.httpkit.server :refer [run-server]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  (def hello-http "Hello http!!!")
+
+(defn app
+  "test fn"
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body hello-http})
+
+(comment
+  (app {})
+  )
+
+(defonce server (atom nil))
+
+(defn stop-server
+  "stop running server"
+  []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(comment
+  (stop-server)
+  )
+
+(defn start-server
+  "start new server instance"
+  []
+  (let [serv (run-server #'app {:port 8080})]
+    (reset! server serv)))
+
+(comment
+  (start-server)
+  )
+
+(defn -main
+  "run the application server."
+  [& args]
+  (start-server)
+  (println "Server has been started on 8080"))
