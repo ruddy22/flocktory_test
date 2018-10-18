036e520788584b55dc565733247d7d14926545a2
Alexey Petrov <ruddy22@mail.ru>
2018-10-19 01:41:49 +0300

    [server]Added `claypoole` to the project

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;; I did that because -> https://www.deps.co/blog/how-to-upgrade-clojure-projects-to-use-java-9/
   :jvm-opts ["--add-modules" "java.xml.bind"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.6.0"]
-                 [http-kit "2.2.0"]])
+                 [http-kit "2.2.0"]
+                 ;; use `claypoole` because -> https://www.youtube.com/watch?v=BzKjIk0vgzE
+                 [com.climate/claypoole "1.1.4"]])
