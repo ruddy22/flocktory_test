1fdb760ee87432a9cfecc62e58726286705a6cd1
Alexey Petrov <ruddy22@mail.ru>
2018-10-20 02:40:29 +0300

    [server]Updated index handler

--- src/flocktory/handlers/index.clj
+++ src/flocktory/handlers/index.clj
@@ -5,7 +5,16 @@
   [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>If you want to use this application, send request on `search` route.</h1>"} ;; TODO: add description with example
+   :body (str "<h1>Greeting page</h1>"
+              "<div>This is a test task for Flocktory.</div>"
+              "<br/>"
+              "<div>Main points:</div>"
+              "<div>1. Send request to Bing Search Engine</div>"
+              "<div>2. Process the response</div>"
+              "<div>3. Receive statistics that based on the response</div>"
+              "<h3>Wanna try?</h3>"
+              "<div>click this link <a href=\"http://localhost:8080/search?query=haskell&query=scala&query=clojure&query=lisp&query=javascript\">http://localhost:8080/search?query=haskell&query=scala&query=clojure&query=lisp&query=javascript</a></div>"
+              )}
   )
 
 
