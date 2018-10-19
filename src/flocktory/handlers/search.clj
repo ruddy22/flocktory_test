78d01f8c23983d70c0b5e52f156530269a872ec0
Alexey Petrov <ruddy22@mail.ru>
2018-10-20 02:17:14 +0300

    [server]Added error handling

--- src/flocktory/handlers/search.clj
+++ src/flocktory/handlers/search.clj
@@ -2,17 +2,37 @@
   (:require [flocktory.libs.threadpool :refer (make-threadpool destroy-threadpool)]
             [flocktory.libs.data-handler :refer (handle-data)]
             [flocktory.libs.rss :refer (get-rss)]
+            [flocktory.libs.json :refer (make-json prettify)]
             ))
+
+(defn make-pos-response
+  "positive response"
+  [data]
+  {:status 200
+   :headers {"Content-Type" "application/json; charset=utf-8"}
+   :body data})
+
+(defn handle-error
+  "negative response"
+  [error]
+  (let [error-str (.toString error)
+        json (-> {:error error-str}
+                 make-json
+                 prettify)]
+    {:status 500
+     :headers {"Content-Type" "application/json; charset=utf-8"}
+     :body json}
+    ))
 
 (defn search-handler
   "search route handler"
   [req]
   (let [{:keys [query-string]} req
-        pool (make-threadpool)
-        res (handle-data query-string pool get-rss)
-        response {:status 200
-                  :headers {"Content-Type" "application/json"}
-                  :body res}]
-    (destroy-threadpool pool)
-    response
+        pool (make-threadpool)]
+    (try
+      (-> query-string
+          (handle-data pool get-rss)
+          make-pos-response)
+      (catch Exception e (handle-error e))
+      (finally (destroy-threadpool pool)))
     ))
