9e7e5fc4cfd98573172411377dc475442967de50
Alexey Petrov <ruddy22@mail.ru>
2018-10-21 01:27:36 +0300

    [done][tests]Tests have been added to the project
    Added tests on exceptions for `data_handler_tests`
    Refactored (a little) `data_handler`

--- src/flocktory/libs/data_handler.clj
+++ src/flocktory/libs/data_handler.clj
@@ -11,8 +11,23 @@
   [query]
   (str "https://www.bing.com/search?q=" query "&format=rss&count=10"))
 
+(defn query-string->words
+  "clean and convert query-string to list of words"
+  [query-string]
+  (-> query-string
+      (s/replace #"query=" "")
+      (s/split #"&")))
+
+(defn domains->json
+  "perform frequencies calculation and convert result to pretty json"
+  [domains]
+  (-> domains
+      frequencies
+      make-json
+      prettify))
+
 (defn process-urls
-  "string -> url -> sld"
+  "get sld"
   [url-str]
   (try
     (let [host (-> url-str
@@ -20,33 +35,34 @@
                    (.getHost))
           sld (take 2 (reverse (s/split host #"\.")))]
       (s/join "." (reverse sld)))
+    (catch java.lang.NullPointerException e
+      (throw (Exception. "null pointer exception")))
+    (catch java.net.MalformedURLException e
+      (throw (Exception. (str (first (s/split (.getMessage e) #":"))))))
     (catch Exception e
-      (throw (Exception. "Unknown error")))
-  ))
+      (throw (Exception. (.getMessage e))))))
 
 (defn process
   "subprocess for each word"
   [get-feed word]
-  (let [entries (-> word
-                    make-url
-                    get-feed
-                    :entries)
-        links (map :link entries)]
-    links))
+  (try
+    (let [entries (-> word
+                      make-url
+                      get-feed
+                      :entries)
+          links (map :link entries)]
+      links)
+    (catch Exception e
+      (throw (Exception. (.getMessage e))))))
 
 (defn data-processing
   "words processing"
   [query-string pool get-feed]
-  (let [words (-> query-string
-                  (s/replace #"query=" "")
-                  (s/split #"(&)"))
+  (let [words (query-string->words query-string)
         union (apply set/union
                      (tp/do-in-thread pool (partial process get-feed) words))
         domains (map process-urls union)
-        json (-> domains
-                 frequencies
-                 make-json
-                 prettify)]
+        json (domains->json domains)]
     json
     ))
 
--- test/flocktory/libs/data_handler_test.clj
+++ test/flocktory/libs/data_handler_test.clj
@@ -7,15 +7,31 @@
     (is (= "https://www.bing.com/search?q=clojure&format=rss&count=10"
            (dh/make-url "clojure")))))
 
+(deftest query-string->words-test
+  (testing "clean and convert query-string to list of strings"
+    (is (= ["clojure" "haskell"]
+           (dh/query-string->words "query=clojure&query=haskell")))))
+
+(deftest domains->json-test
+  (testing "perform frequencies calculation and convert result to pretty json"
+    (is (= "{\n\t\"clojure.org\":2\n}\n"
+           (dh/domains->json '("clojure.org" "clojure.org"))))))
+
 (deftest process-urls-test
-  (testing "string -> url -> sld"
+  (testing "get sld"
     (is (= '("wiki.com" "wiki.com" "wiki.com")
            (map dh/process-urls (list "http://en.wiki.com" "https://es.us.wiki.com" "http://ru-ru.wiki.com")))))
-  (testing "exceptions"
-    (testing "Unknown exception"
-      (is (thrown? Exception (dh/process-urls nil)))
-      (is (thrown-with-msg? Exception #"Unknown error" (dh/process-urls nil)))
-      )))
+  (testing "get exceptions"
+    (testing "Null Pointer Exception"
+      (is (thrown? java.lang.Exception (dh/process-urls nil)))
+      (is (thrown-with-msg? java.lang.Exception #"null pointer exception" (dh/process-urls nil))))
+    (testing "Wrong Protocol"
+      (is (thrown? java.lang.Exception (dh/process-urls "wap://en.wiki.com")))
+      (is (thrown-with-msg? java.lang.Exception #"unknown protocol" (dh/process-urls "wap://en.wiki.com"))))
+    (testing "No Protocol"
+      (is (thrown? java.lang.Exception (dh/process-urls "en.wiki.com")))
+      (is (thrown-with-msg? java.lang.Exception #"no protocol" (dh/process-urls "en.wiki.com"))))
+    ))
 
 (defn fake-get-feed [_]
   {:entries (list {:link "1"}
