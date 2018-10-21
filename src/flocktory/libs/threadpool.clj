e9c6db83c9e3ac1a9dd988eb94f7049307e8a1d8
Alexey Petrov <ruddy22@mail.ru>
2018-10-21 16:13:28 +0300

    [threadpool]Fn descriptions have been updated

--- src/flocktory/libs/threadpool.clj
+++ src/flocktory/libs/threadpool.clj
@@ -4,17 +4,31 @@
 (def default-pool-size 10)
 
 (defn make-threadpool
-  "create a threadpool"
+  "
+  int? -> instance
+
+  Takes `size` (if specified) and creates a thread pool executor
+  with `size` processes.
+  "
   ([] (make-threadpool default-pool-size))
   ([size] (cp/threadpool size)))
 
 (defn destroy-threadpool
-  "destroy a threadpool"
+  "
+  instance -> nil
+
+  Takes a `pool` and destroy it.
+  "
   [pool]
   (cp/shutdown pool))
 
 (defn do-in-thread
-  "do something in parallel"
+  "
+  instance -> fn -> data -> *
+
+  Takes `pool`, `fn`, `data` and parallelizes the execution of `fn` with `data`
+  on the processes from the pool.
+  "
   [pool fn data]
   (cp/pmap pool fn data))
 
